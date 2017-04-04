9a67808e076cc1ba4f3221a2e92718b584427f5c
lexie <asi8443@rit.edu>
2017-04-04 18:06:36 -0400

    donor registration pages

--- donatoro.py
+++ donatoro.py
@@ -24,6 +24,19 @@ def charityRegistrationOne():
 def charityRegistration2():
     return render_template('registration/charityRegistration/registrationPage2.jinja')
 
+@app.route('/charityConfirmation')
+def charityConfirmation():
+    charityInformation = {"name":"Fake Charity", "501c":"123456789", "tags": "Environment", "email":"fakeCharity@fakeCharity.com", "billing": "1 Main St, Rochester, NY, 14623", "description":"This organization is aimed to plant 50,000 new trees every year, as well as replant trees in areas across the country affected by natural disasters. All trees planted will be native to the area they are planted in to avoid introducing invasive species."}
+    return render_template('registration/charityRegistration/confirmation.jinja', charityInfo = charityInformation)
+
+@app.route('/donorRegistration')
+def donorRegistration():
+    return render_template('registration/donorRegistration/donorRegistration.jinja')
+
+@app.route('/donorBilling')
+def donorBilling():
+    return render_template('registration/donorRegistration/donorBilling.jinja')
+
 if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run()
