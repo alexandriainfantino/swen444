9ee582f1940b65f022806502b9a493285d878145
Joshua Jarvis <jmj3886@rit.edu>
2017-04-11 21:28:46 -0400

    #17 Added charity home page

--- donatoro.py
+++ donatoro.py
@@ -49,6 +49,12 @@ def donor_edit_payment():
 	fakeCards = ["5678","6666"]
 	return render_template('DonorProfile/donorPayment.jinja', username="Johnny Bravo", cards=fakeCards)
 
+@app.route('/charityHome')
+def charity_home():
+    info="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris dui lorem, porta sit amet tellus non, semper blandit augue. Vivamus vitae ligula quis neque venenatis blandit eget nec sapien. Duis sodales orci in est feugiat lobortis. Integer non purus sem. Vestibulum eu fermentum tellus, eu vulputate leo. Aliquam vitae est nec diam ornare efficitur. Aenean congue fermentum justo, aliquam porta velit consequat nec. Duis ac tortor metus.."
+    pics=['tree1.jpg','tree2.jpg','tree3.jpg','tree4.jpg']
+    return render_template('charityHome.jinja', username="Johnny Bravo", charityName="I LUV TREES", info=info, pics=pics)
+
 @app.route('/')
 def log_in():
     return render_template('logIn.jinja')
