06ead871d1f3d98c7317b688c24104d62d6d1a55
Joshua Jarvis <jmj3886@rit.edu>
2017-04-15 06:03:46 -0400

    #24 Added charity profile database connects, both pull and actions

--- donatoro.py
+++ donatoro.py
@@ -6,42 +6,45 @@ app = Flask(__name__)
 states=["Alabama" ,"Alaska" ,"Arizona" ,"Arkansas" ,"California" ,"Colorado","Connecticut","Delaware" ,"Florida","Georgia" ,"Hawaii" ,"Idaho","Illinois", "Indiana" ,"Iowa" ,"Kansas" ,"Kentucky" ,"Louisiana" ,"Maine" ,"Maryland" ,"Massachusetts" ,"Michigan" ,"Minnesota" ,"Mississippi" ,"Missouri" ,"Montana", "Nebraska" ,"Nevada" ,"New Hampshire" ,"New Jersey" ,"New Mexico" ,"New York" ,"North Carolina" ,"North Dakota" ,"Ohio" ,"Oklahoma" ,"Oregon","Pennsylvania", "Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virginia","Washington","West Virginia","Wisconsin" ,"Wyoming"]
 countries=["Afghanistan","Albania","Algeria","American Samoa","Andorra","Angola","Anguilla","Antigua and Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","The Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia and Herzegovina","Botswana","Brazil","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Cayman Islands","Central African Republic","Chad","Chile","People 's Republic of China","Republic of China","Christmas Island","Cocos(Keeling) Islands","Colombia","Comoros","Congo","Cook Islands","Costa Rica","Cote d'Ivoire","Croatia","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","Gabon","The Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guadeloupe","Guam","Guatemala","Guernsey","Guinea","Guinea - Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kiribati","North Korea","South Korea","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Martinique","Mauritania","Mauritius","Mayotte","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Myanmar","Nagorno - Karabakh","Namibia","Nauru","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Niue","Norfolk Island","Turkish Republic of Northern Cyprus","Northern Mariana","Norway","Oman","Pakistan","Palau","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Pitcairn Islands","Poland","Portugal","Puerto Rico","Qatar","Romania","Russia","Rwanda","Saint Barthelemy","Saint Helena","Saint Kitts and Nevis","Saint Lucia","Saint Martin","Saint Pierre and Miquelon","Saint Vincent and the Grenadines","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","Somaliland","South Africa","South Ossetia","Spain","Sri Lanka","Sudan","Suriname","Svalbard","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor - Leste","Togo","Tokelau","Tonga","Transnistria Pridnestrovie","Trinidad and Tobago","Tristan da Cunha","Tunisia","Turkey","Turkmenistan","Turks and Caicos Islands","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","British Virgin Islands","Isle of Man","US Virgin Islands","Wallis and Futuna","Western Sahara","Yemen","Zambia","Zimbabwe"]
 
-
+testUser = 2
 
 @app.route('/profile', methods = ['GET', 'POST'])
 def profile():
-    user = getUserById(1)
+    user = getUserById(testUser)
     if user["isDonor"] == 1:        
-        info = getDonorInfoByUserId(1)
+        info = getDonorInfoByUserId(testUser)
         return render_template('DonorProfile/donorProfile.jinja', username=(info["firstName"]+" "+info["lastName"]), email=user["email"], address=info["streetAdd1"], address2=info["streetAdd2"], city=info["city"], state=info["state"], zipcode=info["zip"])
     else:
-        return render_template('CharityProfile/charityProfile.jinja', username="Charity Name", email="charity@gmail.com", address="123 Rainbow Drive", city="Rochester", state="New York", zipcode="14623", country="United States")
+        info = getCharityInfoByUserId(testUser)
+        return render_template('CharityProfile/charityProfile.jinja', username=info["name"], email=user["email"], address=info["streetAdd"], city=info["city"], state=info["state"], zipcode=info["zip"], country=info["country"])
 
 @app.route('/personal')
 def edit_personal():
-    user = getUserById(1)
+    user = getUserById(testUser)
     if user["isDonor"] == 1:        
-        info = getDonorInfoByUserId(1)
+        info = getDonorInfoByUserId(testUser)
         return render_template('DonorProfile/donorPersonal.jinja', username=(info["firstName"]+" "+info["lastName"]), first_name=info["firstName"], last_name=info["lastName"], email=user["email"], address=info["streetAdd1"], address2=info["streetAdd2"], city=info["city"], state=info["state"], zipcode=info["zip"], states=states)
     else:
-        return render_template('CharityProfile/charityPersonal.jinja', username="Charity Name", email="charity@gmail.com", address="123 Rainbow Drive", city="Rochester", state="New York", zipcode="14623", country="United States", states=states, countries=countries)
+        info = getCharityInfoByUserId(testUser)
+        return render_template('CharityProfile/charityPersonal.jinja', username=info["name"], email=user["email"], address=info["streetAdd"], city=info["city"], state=info["state"], zipcode=info["zip"], country=info["country"], states=states, countries=countries)
   
 @app.route('/password')
 def edit_password():
-    user = getUserById(1)
+    user = getUserById(testUser)
     if user["isDonor"] == 1:
-        info = getDonorInfoByUserId(1)
+        info = getDonorInfoByUserId(testUser)
         return render_template('password.jinja', username=(info["firstName"]+" "+info["lastName"]), message="")
     else:
-        return render_template('password.jinja', username="Charity Name", message="")
+        info = getCharityInfoByUserId(testUser)
+        return render_template('password.jinja', username=info["name"], message="")
 
 @app.route('/payment')
 def donor_edit_payment():
-    user = getUserById(1)
+    user = getUserById(testUser)
     if user["isDonor"] == 1:
-        info = getDonorInfoByUserId(1)  
+        info = getDonorInfoByUserId(testUser)  
         cards = []      
-        for card in getCreditCardByUserId(1):
+        for card in getCreditCardByUserId(testUser):
             cards.append(dict(num=str(card["ccn"])[-4:],id=card["id"]))
         return render_template('DonorProfile/donorPayment.jinja', username=(info["firstName"]+" "+info["lastName"]), cards=cards, states=states)
 
@@ -49,21 +52,27 @@ def donor_edit_payment():
 #FORM ACTIONS  
 @app.route('/savePersonal', methods = ['POST'])
 def save_personal():   
-    editDonorPersonalInfo("Johnny.Bravo@gmail.com", request.form["emailInput"], request.form["firstName"], request.form["lastName"], request.form["streetAddress1"], request.form["streetAddress2"], request.form["city"], request.form["state"], request.form["zip"]) 
+    user = getUserById(testUser)
+    if user["isDonor"] == 1: 
+        editDonorPersonalInfo(user["email"], request.form["emailInput"], request.form["firstName"], request.form["lastName"], request.form["streetAddress1"], request.form["streetAddress2"], request.form["city"], request.form["state"], request.form["zip"]) 
+    else:
+        editCharityPersonalInfo(user["email"], request.form["emailInput"], request.form["streetAddress1"], request.form["city"], request.form["state"], request.form["zip"], request.form["country"]) 
     return redirect('../profile')
 
 @app.route('/savePassword', methods = ['POST'])
 def save_password():
-    user = getUserById(1)
+    user = getUserById(testUser)
+    print(request.form["oldpassword"]+" - "+user["password"])
     if request.form["oldpassword"] == user["password"]:
-        editPassword("Johnny.Bravo@gmail.com",request.form["newpassword"])
+        editPassword(user["email"],request.form["newpassword"])
         return redirect('../profile')
     else:
         if user["isDonor"] == 1:
-            info = getDonorInfoByUserId(1)
+            info = getDonorInfoByUserId(testUser)
             return render_template('password.jinja', username=(info["firstName"]+" "+info["lastName"]), message="Password Does Not Match Current Password!")
         else:
-            return render_template('password.jinja', username="Charity Name", message="Password Does Not Match Current Password!")
+            info = getCharityInfoByUserId(testUser)
+            return render_template('password.jinja', username=info["name"], message="Password Does Not Match Current Password!")
 
 @app.route('/savePayment', methods = ['POST'])
 def save_payment():
