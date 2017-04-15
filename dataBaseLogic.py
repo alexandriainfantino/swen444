412d6e9a2f7bd76e172de0caa1d65642db2ffadb
Joshua Jarvis <jmj3886@rit.edu>
2017-04-15 02:12:31 -0400

    #23 Fixed list returns for single items in database logic

--- dataBaseLogic.py
+++ dataBaseLogic.py
@@ -1,7 +1,7 @@
 from flask import Flask, g
 import sqlite3
 
-DATABASE = 'C:\\Users\\alexa\\PycharmProjects\\swen444\\donatoro.sqlite'
+DATABASE = 'C:\\Users\\Joshua\\Desktop\\swen444\\donatoro.sqlite'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -22,19 +22,21 @@ def query_db(query, args=(), one=False):
     cur.close()
     return test
 
+def edit_db(statement, args=()):
+    conn = get_db()
+    conn.execute(statement, args)
+    conn.commit()
+
 def getUsers():
     return [dict(id=row[0], password=row[1], email=row[2], isDonor=row[3]) for row in query_db('select * from user').fetchall()]
 
 def getUserById(userId):
-    user = query_db('select * from user where id = ?', (userId,)).fetchone()
-    user = [dict(id=user[0], password=user[2].encode('ascii', 'ignore'), email=user[1].encode('ascii', 'ignore'), isDonor=user[3])]
-    return user
+    user = query_db('select * from user where id=?', (userId,)).fetchone()
+    return dict(id=user[0], password=user[2].encode('ascii', 'ignore'), email=user[1], isDonor=user[3])
 
 def getUserByEmail(email):
-    user = query_db('select * from user where email = ?', (email,)).fetchone()
-    user = [dict(id=user[0], password=user[2], email=user[1],
-                 isDonor=user[3])]
-    return user
+    user = query_db('select * from user where email=?', (email,)).fetchone()
+    return dict(id=user[0], password=user[2], email=user[1], isDonor=user[3])
 
 def getCreditCardByUserId(userId):
     return [dict(ccn=row[0], ccv=row[1], expMonth=row[2], expYear=row[3], streetAdd1 =row[4], streetAdd2=row[5], city=row[6],
@@ -42,22 +44,20 @@ def getCreditCardByUserId(userId):
              query_db('select * from cc where userId=?', (userId,)).fetchall()]
 
 def getCharityInfoByCharityId(charityId):
-    charity = query_db('select * from charity_info where charID = ?', (charityId,)).fetchone()
-    charity = [dict(name=charity[0], identificationNum=charity[1], description=charity[2], streetAdd = charity[3],
-                 city=charity[4], state=charity[5], zip=charity[6], userId=charity[7], charId=charity[8])]
-    return charity
+    charity = query_db('select * from charity_info where charID=?', (charityId,)).fetchone()
+    return dict(name=charity[0], identificationNum=charity[1], description=charity[2], streetAdd = charity[3],
+                 city=charity[4], state=charity[5], zip=charity[6], userId=charity[7], charId=charity[8])
 
 def getDonorInfoByUserId(userId):
-    donor = query_db('select * from donor_info where userID = ?', (userId,)).fetchone()
-    donor = [dict(firstName=donor[0], lastName=donor[1], streetAdd1=donor[2], streetAdd2=donor[3],
-                    city=donor[4], state=donor[5], zip=donor[6], userId=donor[7])]
-    return donor
+    donor = query_db('select * from donor_info where userID=?', (userId,)).fetchone()
+    return dict(firstName=donor[0], lastName=donor[1], streetAdd1=donor[2], streetAdd2=donor[3],
+                    city=donor[4], state=donor[5], zip=donor[6], userId=donor[7])
 
 def getDonorFavorites(userId):
     return [dict(name=row[0], identificationNum=row[1], description=row[2], streetAdd = row[3],
                  city=row[4], state=row[5], zip=row[6], country=row[7], userId=row[8], charId=row[9]) for row in
             query_db('select * from charity_info inner join favorites on charity_info.charID = favorites.charityId '
-                     'where favorites.userId = ?', (userId,)).fetchall()]
+                     'where favorites.userId=?', (userId,)).fetchall()]
 
 def getCharityPosts(charityId):
     return [dict(date=row[0], title=row[1], post=row[2], charId=row[3], Id=row[4]) for row in
@@ -71,4 +71,25 @@ def getCharitiesByTags(tag):
     return [dict(name=row[0], identificationNum=row[1], description=row[2], streetAdd = row[3],
                  city=row[4], state=row[5], zip=row[6], userId=row[7], charId=row[8]) for row in
             query_db('select * from charity_info inner join tags on charity_info.charID = tags.charityId where '
-                     'tags.tag = ?', (tag,)).fetchall()]
+                     'tags.tag=?', (tag,)).fetchall()]
+
+def editDonorPersonalInfo(old_email, email, firstName, lastName, streetAdd1, streetAdd2, city, state, zip):
+    edit_db('UPDATE user SET email=? WHERE email=?', (email, old_email,))
+    user = getUserByEmail(email)
+    edit_db('UPDATE donor_info SET firstName=?, lastName=?, streetAdd1=?, streetAdd2=?, city=?, state=?, zip=? WHERE userId=?', (firstName, lastName, streetAdd1, streetAdd2, city, state, zip , user["id"],))
+
+def editCharityPersonalInfo(old_email, email, firstName, lastName, streetAddress, city, state, zip, country):
+    edit_db('UPDATE user SET email=? WHERE email=?', (email , old_email))
+    user = getUserByEmail(email)
+    edit_db('UPDATE charity_info SET streetAddress=?, city= ?, state=?, zip=?, country=? WHERE userId=?', (sreetAddress, city, state, zip , user["id"],))
+
+def editPassword(email, password):
+    edit_db('UPDATE user SET password=? WHERE email=?', (password , email,))
+
+def deleteCard(email, cardId):
+    user = getUserByEmail(email)
+    edit_db('DELETE FROM cc WHERE userId=? AND id=?', (user["id"], cardId,))
+
+def addCreditCard(email, cardNumber, ccv, exp_month, exp_year, streetAdd1, streetAdd2, city, state, zip):
+    user = getUserByEmail(email)
+    edit_db('INSERT INTO cc (ccn,ccv,expMonth,expYear,streetAdd1,streetAdd2,city,state,zip,userID) VALUES (?,?,?,?,?,?,?,?,?,?)', (cardNumber,ccv,exp_month,exp_year,streetAdd1,streetAdd2,city,state,zip,user["id"],))
